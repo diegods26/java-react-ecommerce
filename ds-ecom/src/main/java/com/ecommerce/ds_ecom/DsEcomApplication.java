4bed0f87ecd4dfb23fb46016906d5eac29878ca4
diegods26 <diegopimenta.ds@hotmail.com>
2025-10-28 23:52:44 -0300

    initial commit

---
+++ ds-ecom/src/main/java/com/ecommerce/ds_ecom/DsEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ds_ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEcomApplication.class, args);
+	}
+
+}
---
+++ ds-ecom/src/test/java/com/ecommerce/ds_ecom/DsEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ds_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
